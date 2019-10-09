a731fd9f0d10b275b821233f9af10d2cf2111d06
Romario Latty <rlatty5@gmail.com>
2019-10-09 19:53:16 -0400

    Repo Workflow

--- Intuit-Github/Controller/LoginViewController.swift
+++ Intuit-Github/Controller/LoginViewController.swift
@@ -10,4 +10,23 @@ import Foundation
 import UIKit
 class LoginViewController: UIViewController{
     
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        let vc:UITabBarController = self.storyboard?.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 }
