8e75e33bf0eebe191b0a178fa1154b216a3a6c8f
Romario Latty <rlatty5@gmail.com>
2019-10-09 09:33:56 -0400

    Initial Commit

---
+++ Intuit-GithubTests/Intuit_GithubTests.swift
@@ -0,0 +1,34 @@
+//
+//  Intuit_GithubTests.swift
+//  Intuit-GithubTests
+//
+//  Created by Romario Latty on 10/9/19.
+//  Copyright © 2019 Romario Latty. All rights reserved.
+//
+
+import XCTest
+@testable import Intuit_Github
+
+class Intuit_GithubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
